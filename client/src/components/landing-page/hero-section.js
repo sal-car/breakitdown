9b06d31a36b99af6c329e6a1ca69f35b0ecf7d4b
Sally Carlund <sally.carlund@gmail.com>
2023-11-14 18:03:55 +0100

    feat: mock login page

--- client/src/components/landing-page/hero-section.js
+++ client/src/components/landing-page/hero-section.js
@@ -7,10 +7,10 @@ import { useRef } from 'react';
 
 
 
-export const HeroSection = function () {
-    const [loginPage, setLoginPage] = useState(false)
+export const HeroSection = function ({setIsAuthenticated}) {
+    const [openLoginPage, setOpenLoginPage] = useState(false)
 
-    const handleClick = function () {
+    const scrollToAbout = function () {
         const about = document.getElementById("about");
         about.scrollIntoView({behavior: "smooth"})
     };
@@ -19,10 +19,14 @@ export const HeroSection = function () {
 
     return (
         <div className="wrapper">
+            {
+                openLoginPage &&
+            <LoginPage setOpenLoginPage={setOpenLoginPage} setIsAuthenticated={setIsAuthenticated}></LoginPage>
+            }
             <div className="HeroSection inset-0 z-0 h-[91vh]">
                 <button className="sticky shadow-md text-3xl h-20 w-44 top-[61.5vh] left-[44vw] bg-[#EA5127] hover:bg-transparent  hover:text-[#EA5127] font-normal font-sans text-white py-2 px-3 border hover:border-[#EA5127]  border-transparent rounded-xl ">Get started</button>
-                <button onClick={handleClick} className="relative shadow-sm bg-transparent text-xl h-14 w-42 px-6 left-5 hover:bg-violet-900 text-violet-900 font-semibold hover:text-white border border-violet-900 hover:border-transparent rounded ">About</button>
-                <button onClick={handleClick} className="relative shadow-sm bg-transparent text-xl h-14 w-42 ml-5 px-6 left-6 hover:bg-violet-900 text-violet-900 font-semibold hover:text-white border border-violet-900 hover:border-transparent rounded ">Log in</button>
+                <button onClick={scrollToAbout} className="relative shadow-sm bg-transparent text-xl h-14 w-42 px-6 left-5 hover:bg-violet-900 text-violet-900 font-semibold hover:text-white border border-violet-900 hover:border-transparent rounded ">About</button>
+                <button onClick={() => setOpenLoginPage(true)}  className="relative shadow-sm bg-transparent text-xl h-14 w-42 ml-5 px-6 left-6 hover:bg-violet-900 text-violet-900 font-semibold hover:text-white border border-violet-900 hover:border-transparent rounded ">Log in</button>
             </div>
             <div id="about" className="About shadow-5xl bg-[#779BF6] h-[89vh] z-20 ">
                 <div className="info absolute w-1/2 flex flex-col justify-center align-middle left-[24vw] top-[105vh]">
--- client/src/components/landing-page/login.js
+++ client/src/components/landing-page/login.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 
-export const LoginPage = function () {
+
+export const LoginPage = function ({setOpenLoginPage, setIsAuthenticated}) {
+    const [formData, setFormData] = useState({
+        name: '',
+        password: ''
+    })
+
+    const handleInputChange = function (e) {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        if (name === 'name') {
+            setFormData({...formData, name: value});
+        } else if (name === 'password') {
+            setFormData({...formData, password: value})
+        }
+    }
+
     return (
-        <div className="bg-white h-[100vh]"></div>
+    <div className='LoginPage overflow-y-auto overflow-x-hidden min-h-[100vh] fixed top-0 right-0 left-0 z-50 justify-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full  bg-gray-600/50'>	
+        <div className="relative p-4 min-w-fit  max-w-fit max-h-full top-[10vh] mx-auto">
+            <div className="relative bg-white min-h-[50vh] rounded-xl shadow px-10 py-8 flex flex-col">
+                <div className="header flex justify-between  mb-10">
+                    <h1 className="create-project-header text-2xl font-semibold">Log in</h1>
+                    <button onClick={() => setOpenLoginPage(false)} className="close-modal">
+                        <CloseIcon style={{color: "#b5b1b1"}} className="absolute top-[15px] right-[15px]"></CloseIcon>
+                    </button>
+                </div>
+                <div className="content flex flex-col gap-10 items-start">
+                    <form className="flex flex-col font-semibold">
+                        <label htmlFor="email">Email</label>
+                        <input onChange={handleInputChange} required name="email" type="email" className="project-name outline-none border p-2.5 rounded-lg w-[400px] mb-3" />
+                        <label htmlFor="password">Password</label>
+                        <input onChange={handleInputChange} required name="password" type="password" className="project-name outline-none border p-2.5 rounded-lg w-[400px] "/>
+                    </form>
+                    <button onClick={() => setIsAuthenticated(true)}  className="relative self-center shadow-sm bg-transparent text-xl h-14 w-42 px-6 hover:bg-violet-900 text-violet-900 font-semibold hover:text-white border border-violet-900 hover:border-transparent rounded ">Log in</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
     )
 }
